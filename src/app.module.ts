e1e302b7f330c3b36a4a045e57a7cd80019a1d25
Fernando Brino <fernandobrino11@gmail.com>
2025-01-29 12:11:30 -0300

    feat: configuration jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
-import { CreateAccountController } from './controllers/create-account.controller';
-import { ConfigModule } from '@nestjs/config';
-import { envSchema } from './env';
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
+import { CreateAccountController } from "./controllers/create-account.controller";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate: env => envSchema.parse(env),
-    isGlobal: true //turns env variables avaiable to all modules
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true, //turns env variables avaiable to all modules
+    }),
+    AuthModule,
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
