f2ebd0260a5bbe91abbaa5ed8fdc30f0273af26f
Fernando Brino <fernandobrino11@gmail.com>
2025-02-02 21:47:10 -0300

    feat: Get user by jwt payload decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Controller, Post, Req, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { Request } from "express";
+import { CurrentUser } from "src/auth/current-user.decorator";
+import { UserPayload } from "src/auth/jwt.strategy";
 
 // const createQuestionBodySchema = z.object({
 //   email: z.string().email(),
@@ -14,8 +17,9 @@ export class CreateQuestionController {
   constructor() {}
 
   @Post()
-  //   @UsePipes(new ZodValidationPipe(authenticateBodySchema))
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
+
     return 'ok'
   }
 }
