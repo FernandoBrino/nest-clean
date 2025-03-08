70e3e7d4bba16fe2b65e093997638dd6099ace85
Fernando Brino <fernandobrino11@gmail.com>
2025-03-08 20:08:24 -0300

    feat: gateway encryption and change file name using dash

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -2,7 +2,7 @@ import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { QuestionComment } from "../../enterprise/entities/question-comment";
-import { QuestionCommentsRepository } from "../repositories/question-comments.repository";
+import { QuestionCommentsRepository } from "../repositories/question-comments-repository";
 import { Either, left, right } from "@/core/either";
 import { ResourceNotFoundError } from "../../../../core/errors/errors/resource-not-found-error";
 
--- src/domain/forum/application/use-cases/delete-question-comment.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -1,5 +1,5 @@
 import { Either, left, right } from "@/core/either";
-import { QuestionCommentsRepository } from "../repositories/question-comments.repository";
+import { QuestionCommentsRepository } from "../repositories/question-comments-repository";
 import { ResourceNotFoundError } from "../../../../core/errors/errors/resource-not-found-error";
 import { NotAllowedError } from "../../../../core/errors/errors/not-allowed-error";
 
