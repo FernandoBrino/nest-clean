c67a5dfa5fc141de7207a30a06bbacac3745e6ad
Fernando Brino <fernandobrino11@gmail.com>
2025-03-20 15:11:16 -0300

    feat(controller): delete question comment

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -30,7 +30,7 @@ export function makeQuestionComment(
 export class QuestionCommentFactory {
   constructor(private prisma: PrismaService) {}
 
-  async makePrismaQuestion(data: Partial<QuestionCommentProps> = {}): Promise<QuestionComment> {
+  async makePrismaQuestionComment(data: Partial<QuestionCommentProps> = {}): Promise<QuestionComment> {
     const questionComment = makeQuestionComment(data);
 
     await this.prisma.comment.create({
