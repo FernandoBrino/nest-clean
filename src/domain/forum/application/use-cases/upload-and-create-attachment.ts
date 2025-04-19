f1caf09680a00e970b20fe5d18b8f9ae9d38681f
Fernando Brino <fernandobrino11@gmail.com>
2025-04-19 16:10:14 -0300

    feat: upload and create attachment use case and uploader contract

--- src/domain/forum/application/use-cases/upload-and-create-attachment.ts
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.ts
@@ -1,12 +1,9 @@
 import { Either, left, right } from "@/core/either";
 import { Injectable } from "@nestjs/common";
-import { Student } from "../../enterprise/entities/student";
-import { HashGenerator } from "../cryptography/hash-generation";
-import { StudentsRepository } from "../repositories/students-repository";
-import { StudentAleradyExistsError } from "./errors/student-already-exists-error";
-import { InvalidAttachmentTypeError } from "./errors/invalid-attachment-type";
 import { Attachment } from "../../enterprise/entities/attachment";
 import { AttachmentsRepository } from "../repositories/attachments-repository";
+import { InvalidAttachmentTypeError } from "./errors/invalid-attachment-type";
+import { Uploader } from "../storage/uploader";
 
 interface UploadAndCreateAttachmentUseCaseRequest {
   fileName: string;
@@ -23,32 +20,35 @@ type UploadAndCreateAttachmentUseCaseResponse = Either<
 
 @Injectable()
 export class UploadAndCreateAttachmentUseCase {
-  constructor(private attachmentsRepository: AttachmentsRepository) {}
+  constructor(
+    private attachmentsRepository: AttachmentsRepository,
+    private uploader: Uploader
+  ) {}
 
   async execute({
     fileName,
     fileType,
-    body
+    body,
   }: UploadAndCreateAttachmentUseCaseRequest): Promise<UploadAndCreateAttachmentUseCaseResponse> {
-    const studentWithSameEmail =
-      await this.studentsRepository.findByEmail(email);
-
-    if (studentWithSameEmail) {
-      return left(new StudentAleradyExistsError(email));
+    if (!/^(image\/(jpeg|png))$|^application\/pdf$/.test(fileType)) {
+      return left(new InvalidAttachmentTypeError(fileType));
     }
 
-    const hashedPassword = await this.hashGenerator.hash(password);
-
-    const student = Student.create({
-      name,
-      email,
-      password: hashedPassword,
+    const { url } = await this.uploader.upload({
+      fileName,
+      fileType,
+      body,
     });
 
-    await this.studentsRepository.create(student);
+    const attachment = Attachment.create({
+      title: fileName,
+      url,
+    });
+
+    await this.attachmentsRepository.create(attachment);
 
     return right({
-      student,
+      attachment,
     });
   }
 }
