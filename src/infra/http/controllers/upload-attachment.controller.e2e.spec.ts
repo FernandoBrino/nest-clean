50f83b94396abafb9a1d233e8b5109177eed36c4
Fernando Brino <fernandobrino11@gmail.com>
2025-04-26 17:22:50 -0300

    test(e2e): update attachment response body

--- src/infra/http/controllers/upload-attachment.controller.e2e.spec.ts
+++ src/infra/http/controllers/upload-attachment.controller.e2e.spec.ts
@@ -36,8 +36,8 @@ describe("Upload attachments (E2E)", () => {
       .attach("file", "./test/e2e/Olivia_Oculos.jpg");
 
     expect(response.statusCode).toBe(201);
-    // expect(response.body).toEqual({
-    //   attachmentId: expect.any(String),
-    // });
+    expect(response.body).toEqual({
+      attachmentId: expect.any(String),
+    });
   });
 });
