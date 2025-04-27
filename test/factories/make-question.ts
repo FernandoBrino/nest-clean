b5c6369b8784e533a7f4a6c8b1d93c0921b8e81c
Fernando Brino <fernandobrino11@gmail.com>
2025-04-27 14:15:50 -0300

    test(unit): create and edit question with attachment

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -5,10 +5,9 @@ import {
   Question,
   QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { PrismaQuestionMapper } from "@/infra/database/prisma/mappers/prisma-question-mapper";
 import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { Injectable } from "@nestjs/common";
-import { PrismaQuestionMapper } from "@/infra/database/prisma/mappers/prisma-question-mapper";
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
@@ -19,7 +18,6 @@ export function makeQuestion(
       authorId: new UniqueEntityId(),
       title: faker.lorem.sentence(),
       content: faker.lorem.text(),
-      slug: Slug.create("example-question"),
       ...override,
     },
     id
