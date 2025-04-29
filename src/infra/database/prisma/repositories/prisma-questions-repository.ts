bbafdf9b08d2ce6e5ee3b67076e9b34d9ca4d130
Fernando Brino <fernandobrino11@gmail.com>
2025-04-28 21:58:26 -0300

    feat(infra): create attachments methods on prisma repository and updating create question controller

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -5,10 +5,14 @@ import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
+import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private questionAttachmentsRepository: QuestionAttachmentsRepository
+  ) {}
 
   async findById(id: string): Promise<Question | null> {
     const question = await this.prisma.question.findUnique({
@@ -56,23 +60,36 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     await this.prisma.question.create({
       data,
     });
+
+    await this.questionAttachmentsRepository.createMany(
+      question.attachments.getItems()
+    );
   }
 
   async save(question: Question): Promise<void> {
     const data = PrismaQuestionMapper.toPersistence(question);
 
-    await this.prisma.question.update({
-      where: {
-        id: data.id
-      },
-      data
-    });
+    await Promise.all([
+      this.prisma.question.update({
+        where: {
+          id: data.id,
+        },
+        data,
+      }),
+      this.questionAttachmentsRepository.createMany(
+        question.attachments.getNewItems()
+      ),
+
+      this.questionAttachmentsRepository.createMany(
+        question.attachments.getRemovedItems()
+      ),
+    ]);
   }
 
   async delete(question: Question): Promise<void> {
     await this.prisma.question.delete({
       where: {
-        id: question.id.toString()
+        id: question.id.toString(),
       },
     });
   }
