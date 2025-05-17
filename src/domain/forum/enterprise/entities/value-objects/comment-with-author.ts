100c6a28f514829c6be061ddbbf623ce7b93bd9b
Fernando Brino <fernandobrino11@gmail.com>
2025-05-17 12:22:44 -0300

    feat(domain): question comment with author

--- src/domain/forum/enterprise/entities/value-objects/comment-with-author.ts
+++ src/domain/forum/enterprise/entities/value-objects/comment-with-author.ts
@@ -1,9 +1,10 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { ValueObject } from "@/core/entities/value-object";
 
 export interface CommentWithAuthorProps {
-  commentId: string;
+  commentId: UniqueEntityId;
   content: string;
-  authorId: string;
+  authorId: UniqueEntityId;
   author: string;
   createdAt: Date;
   updatedAt?: Date | null;
@@ -13,7 +14,7 @@ export class CommentWithAuthor extends ValueObject<CommentWithAuthorProps> {
   get commentId() {
     return this.props.commentId;
   }
-  
+
   get content() {
     return this.props.content;
   }
